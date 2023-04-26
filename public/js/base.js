d3cca44fbabe6992c0a5318e95fe6b0f421d1c93
Matheus Clemente Senna <mclementesenna@hotmail.com>
2023-04-26 01:51:02 -0300

    Iniciando o GIT

---
+++ public/js/base.js
@@ -0,0 +1,92 @@
+const nome = "Marcelo Eltz";
+let nome2 = "";
+let pessoaDefault = {
+    nome: "Marcelo Eltz",
+    idade: "33",
+    trabalho: "Programador"
+}
+
+let nomes = ["Marcelo Eltz", "Maria Silva", "Pedro Silva"]
+
+let pessoasListaVazia = [];
+
+let pessoas = [
+    {
+        nome: "Marcelo Eltz",
+        idade: "33",
+        trabalho: "Programador"
+    },
+    {
+        nome: "Maria Silva",
+        idade: "25",
+        trabalho: "UX/UI Designer"
+    },
+];
+
+
+function alterarNome(){
+    nome2 = "Maria Silva"
+    console.log("Valor alterado:");
+    console.log(nome2);
+}
+
+function recebeEalteraNome(novoNome){
+    nome2 = novoNome;
+    console.log("Valor alterado recebendo um nome");
+    console.log(nome2);
+}
+
+function imprimirPessoa(pessoa) {
+    console.log("Nome:");
+    console.log(pessoa.nome);
+
+    console.log("Idade:");
+    console.log(pessoa.idade);
+
+    console.log("Trabalho:");
+    console.log(pessoa.trabalho);
+}
+
+
+function adicionarPessoa(pessoa){
+    pessoas.push(pessoa);
+}
+
+function imprimirPessoas() {
+    console.log("-----------------IMPRIMIR PESSOAS-------------------");
+    pessoas.forEach((item) => {
+        console.log("Nome:");
+        console.log("item.nome");
+
+        console.log("Idade:");
+        console.log(item.idade);
+    
+        console.log("Trabalho:");
+        console.log(item.trabalho);
+    })
+}
+
+imprimirPessoas()
+
+console.log(pessoas);
+
+adicionarPessoa({
+    nome: "Pedro Silva",
+    idade: "28",
+    trabalho: "Porteiro"
+});
+
+console.log(pessoas);
+
+//imprimirPessoa(pessoaDefault);
+
+//imprimirPessoa({
+//    nome: "Maria Silva",
+//    idade: "25",
+//    trabalho: "UX/UI Desing"
+//})
+
+//recebeEalteraNome("João Silva Pereira");
+//recebeEalteraNome("Maria Silva");
+
+//alterarNome();
